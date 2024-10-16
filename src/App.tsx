61f94db080a408f512288b52bdb3341d693388cc
renegildo <renegildo@proton.me>
2024-10-16 13:31:12 -0300

    feat: add better animations and user experience while transitioning between pages

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ type Page = "home" | "food" | "retail" | "community";
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState<Page>("home");
+  const [isTransitioning, setIsTransitioning] = useState<boolean>(false);
 
   useGSAP(() => {
     const timeline = gsap.timeline();
@@ -26,14 +27,26 @@ const App = () => {
   }, []);
 
   const updateCurrentPage = (newPage: Page) => {
-    if (newPage === currentPage) return;
+    if (newPage === currentPage || isTransitioning) return;
+
+    setIsTransitioning(true);
 
     const timeline = gsap.timeline();
+
+    timeline.to(`.${currentPage} > .button-container`, {
+      opacity: 1,
+      duration: 0.2,
+    });
 
     timeline.to(`.${currentPage} > .content`, {
       opacity: 0,
       duration: 0.2,
     });
+
+    timeline.to(`.${newPage} > .button-container`, {
+      opacity: 0,
+      duration: 0.2,
+    }, "-=0.2");
 
     timeline.to(`.${currentPage}`, {
       width: 64,
@@ -54,119 +67,126 @@ const App = () => {
       },
       "-=0.49"
     );
+
+    setIsTransitioning(false);
   };
 
   return (
     <nav className="fixed h-screen right-0 text-xl flex bg-black w-screen">
       <div
-        className="font-mono min-w-16 bg-white h-full flex flex-col items-center pt-20 vertical-item -translate-y-[100%] home"
+        className="font-mono min-w-16 bg-white h-full pt-20 vertical-item -translate-y-[100%] home"
         onClick={() => updateCurrentPage("home")}
       >
-        {currentPage === "home" ? (
-          <div className="content opacity-0">
-            <div className="m-20 mr-64">
-              <a href="/">
-                <img src={logo} />
-              </a>
-              <h1 className="text-7xl font-ogg mt-32 max-w-[1030px]">
-                Canal Street Market is a carefully curated retail market, food hall {"&"}
-                community space open year-round at 265 Canal Street. Support Small Business this weekend!
-              </h1>
-            </div>
-
-            <img src={heroImage} />
+        <div
+          className="content opacity-0"
+          style={{display: currentPage === "home" ? "block" : "none"}}
+        >
+          <div className="m-20 mr-64">
+            <a href="/">
+              <img src={logo} />
+            </a>
+            <h1 className="text-7xl font-ogg mt-32 max-w-[1030px]">
+              Canal Street Market is a carefully curated retail market, food hall {"&"}
+              community space open year-round at 265 Canal Street. Support Small Business this weekend!
+            </h1>
           </div>
-        ) : (
-        <>
+
+          <img src={heroImage} />
+        </div>
+
+        <div
+          className="flex flex-col items-center h-full button-container"
+          style={{display: currentPage !== "home" ? "flex" : "none"}}
+        >
           <p>餐饮</p>
           <span className="[writing-mode:vertical-lr] my-auto">
             Home
           </span>
-        </>
-        )}
+        </div>
       </div>
 
       <div
-        className="font-mono min-w-16 bg-cyan h-full flex flex-col items-center pt-20 vertical-item -translate-y-[100%] food"
+        className="font-mono min-w-16 bg-cyan h-ful pt-20 vertical-item -translate-y-[100%] food"
         onClick={() => updateCurrentPage("food")}
       >
-        {currentPage === "food" ? (
-          <div className="content opacity-0">
-            <div className="m-20 mr-64">
-              <a href="/">
-                <img src={logo} />
-              </a>
-              <h1 className="text-7xl font-ogg mt-32 max-w-[1030px]">
-                Food content
-              </h1>
-            </div>
-
-            <img src={heroImage} />
+        <div
+          style={{display: currentPage === "food" ? "block" : "none"}}
+          className="content opacity-0"
+        >
+          <div className="m-20 mr-64">
+            <h1 className="text-7xl font-ogg mt-32 max-w-[1030px]">
+              Food content
+            </h1>
           </div>
-        ) : (
-        <>
+
+          <img src={heroImage} />
+        </div>
+
+        <div
+          className="flex flex-col items-center h-full button-container"
+          style={{display: currentPage !== "food" ? "flex" : "none"}}
+        >
           <p>餐饮</p>
           <span className="[writing-mode:vertical-lr] my-auto">
             Food
           </span>
-        </>
-        )}
+        </div>
       </div>
 
       <div
-        className="font-mono min-w-16 bg-red h-full flex flex-col items-center pt-20 vertical-item -translate-y-[100%] retail"
+        className="font-mono min-w-16 bg-red h-full pt-20 vertical-item -translate-y-[100%] retail"
         onClick={() => updateCurrentPage("retail")}
       >
-        {currentPage === "retail" ? (
-          <div className="content opacity-0">
-            <div className="m-20 mr-64">
-              <a href="/">
-                <img src={logo} />
-              </a>
-              <h1 className="text-7xl font-ogg mt-32 max-w-[1030px]">
-                Canal Street Market is a carefully curated retail market, food hall {"&"}
-                community space open year-round at 265 Canal Street. Support Small Business this weekend!
-              </h1>
-            </div>
-
-            <img src={heroImage} />
+        <div
+          className="content opacity-0"
+          style={{display: currentPage === "retail" ? "block" : "none"}}
+        >
+          <div className="m-20 mr-64">
+            <h1 className="text-7xl font-ogg mt-32 max-w-[1030px]">
+              Retail content
+            </h1>
           </div>
-        ) : (
-          <>
-            <p>購物</p>
-            <span className="[writing-mode:vertical-lr] my-auto">
-              Retail
-            </span>
-          </>
-        )}
+
+          <img src={heroImage} />
+        </div>
+
+        <div
+          className="flex flex-col items-center h-full button-container"
+          style={{display: currentPage !== "retail" ? "flex" : "none"}}
+        >
+          <p>購物</p>
+          <span className="[writing-mode:vertical-lr] my-auto">
+            Retail
+          </span>
+        </div>
       </div>
 
       <div
-        className="font-mono min-w-16 bg-yellow h-full flex flex-col items-center pt-20 vertical-item -translate-y-[100%] community"
+        className="font-mono min-w-16 pt-20 bg-yellow h-full vertical-item -translate-y-[100%] community"
         onClick={() => updateCurrentPage("community")}
       >
-        {currentPage === "community" ? (
-          <div className="content opacity-0">
-            <div className="m-20 mr-64">
-              <a href="/">
-                <img src={logo} />
-              </a>
-              <h1 className="text-7xl font-ogg mt-32 max-w-[1030px]">
-                Canal Street Market is a carefully curated retail market, food hall {"&"}
-                community space open year-round at 265 Canal Street. Support Small Business this weekend!
-              </h1>
-            </div>
-
-            <img src={heroImage} />
+        <div
+          className="content opacity-0" 
+          style={{display: currentPage === "community" ? "block" : "none"}}
+        >
+          <div className="m-20 mr-64">
+            <h1 className="text-7xl font-ogg mt-32 max-w-[1030px]">
+              Community content
+            </h1>
           </div>
-        ) : (
-          <>
-            <p>文化</p>
-            <span className="[writing-mode:vertical-lr] my-auto">
-              Community
-            </span>
-          </>
-        )}
+
+          <img src={heroImage} />
+        </div>
+
+        <div
+          className="h-full flex flex-col items-center justify-center button-container"
+          style={{display: currentPage !== "community" ? "flex" : "none"}}
+        >
+          <p>文化</p>
+          <span className="[writing-mode:vertical-lr] my-auto">
+            Community
+          </span>
+        </div>
       </div>
     </nav>
   );
