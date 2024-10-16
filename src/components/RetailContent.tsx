4e1ff0c18abcb71f74e8faeb516b82756399c0fb
renegildo <renegildo@proton.me>
2024-10-16 13:55:29 -0300

    feat: add basic retail content

--- src/components/RetailContent.tsx
+++ src/components/RetailContent.tsx
@@ -1,4 +1,5 @@
 import { Page } from "../types";
+import retailImage from "/src/assets/images/retail-image.avif";
 
 const RetailContent = ({
   currentPage
@@ -11,9 +12,15 @@ const RetailContent = ({
       style={{display: currentPage === "retail" ? "block" : "none"}}
     >
       <div className="m-20 mr-64">
-        <h1 className="text-7xl font-ogg mt-32 max-w-[1030px]">
-          Retail content
-        </h1>
+        <div className="flex gap-20 items-center relative">
+          <h1 className="text-[10rem] leading-[1] w-96 font-ogg mt-32 max-w-[1030px] font-bold">
+            The Retail Market
+          </h1>
+
+          <img src={retailImage} className="h-[550px]" />
+
+          <h2 className="text-7xl absolute top-0 -right-56">購物</h2>
+        </div>
       </div>
     </div>
   );
